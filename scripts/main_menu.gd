ffca1af6a44651cbf4ddb3e7388e881531b2caaa
kolbyheiss@gmail.com <kolbyheiss@gmail.com>
2024-03-14 16:35:38 -0700

    Added buttons to main menu, added Fire Hydrant object to game

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -8,10 +8,22 @@ func _ready():
 func _process(delta):
 	#Attempted to animate main quail replace with tweens?
 	main_quail.position.x += 50.0 * delta
+	if main_quail.position.x > 150.0:
+		main_quail.position = Vector2(-150,40)
+	else:
+		pass
 	#await get_tree().create_timer(1000).timeout
 	#main_quail.position.x = 0.0
 	#baby_quail_left.flip_h = true
 	#await get_tree().create_timer(1000).timeout
 	#baby_quail_left.flip_h = false
 	if Input.is_action_pressed("move_down"):
-		get_tree().change_scene_to_file("res://world.tscn")
+		pass
+
+
+func _on_button_button_down():
+	get_tree().change_scene_to_file("res://world.tscn")
+
+
+func _on_button_2_button_down():
+	get_tree().quit()
