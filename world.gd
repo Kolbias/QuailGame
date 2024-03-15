3bf3d35f5ede5159f3ac4fc243b01e64fb0c961e
kolbyheiss@gmail.com <kolbyheiss@gmail.com>
2024-03-15 10:39:31 -0700

    Added Z ordering to objects, added scrolling background, updated map, added end flag without code

--- world.gd
+++ world.gd
@@ -2,8 +2,13 @@ extends Node2D
 
 @onready var main_quail = get_node("Player")
 @onready var quail_baby = preload("res://quail_baby.tscn")
+@onready var parallax = $ParallaxBackground/ParallaxLayer
+@export var background_scroll_speed = Vector2(0.15,0.15)
 
 func _on_quail_egg_quail_hatched():
 	var new_quail = quail_baby.instantiate()
 	add_child(new_quail)
 	new_quail.position = main_quail.position
+
+func _process(delta):
+	parallax.motion_offset += background_scroll_speed
