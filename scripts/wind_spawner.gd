94aa74abb2faf875b9d1a9c2ffebc111e04eb5dc
kolbyheiss@gmail.com <kolbyheiss@gmail.com>
2024-03-21 12:16:10 -0700

    Altered Wind Spawner to work somewhat, now spawner correctly spawns wind and moves but wind is anchored on spawners position, need work around

--- scripts/wind_spawner.gd
+++ scripts/wind_spawner.gd
@@ -6,23 +6,40 @@ extends Node2D
 @export var time_max = 20
 @onready var random_time = randi_range(time_min, time_max)
 @onready var active = false
+@onready var random_y_pos = 0.0
+@onready var parent = get_parent()
 
 func _ready():
 	spawn_wind()
+	timer.start()
 	
 	
 
 func _process(delta):
-	pass
+	if position.y > 1000:
+		position.y = 0
+		get_new_y()
+	if position.y < -1000:
+		position.y = 0
+		get_new_y()
 		
 
 # None of this works :(
 func _on_timer_timeout():
-	active = true
-	active = false
+	spawn_wind()
+	get_new_y()
+	move()
 
 func spawn_wind():
-	while active:
-		var instance = wind.instantiate()
-		add_child(instance)
-		continue
+	var instance = wind.instantiate()
+	add_child(instance)
+	print("Wind Spawned")
+	timer.start()
+	print("Timer restarted")
+
+func move():
+	position.y = random_y_pos
+	
+func get_new_y():
+	random_y_pos = randf_range(-200,200)
+	
