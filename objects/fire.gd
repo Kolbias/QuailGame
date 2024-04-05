8dc367c85e4ab11eec6dc70ad073f0dec6be44dc
Kolby <kolbyheiss@gmail.com>
2024-04-04 22:26:07 -0700

    Edited Fire added alternate shader colors

--- objects/fire.gd
+++ objects/fire.gd
@@ -4,7 +4,7 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
+	print_rich("[color=RED][pulse]Incoming Fire![/pulse]")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
