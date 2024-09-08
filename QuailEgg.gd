ad436b88ad8ee627416a19ec774aad72413fa29c
kolbyheiss@gmail.com <kolbyheiss@gmail.com>
2024-09-08 15:48:39 -0700

    Reworked fences in world1 may need further tweaking

--- QuailEgg.gd
+++ QuailEgg.gd
@@ -5,14 +5,6 @@ extends Area2D
 @onready var particles = $EggHatchParticles
 @onready var sprite = $Sprite2D
 signal quail_hatched
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
 
 func _on_area_entered(area):
 	if area.is_in_group("player"):
