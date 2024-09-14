78ec17daae30a2ace859f3000eddbc116debb0a5
kolbyheiss@gmail.com <kolbyheiss@gmail.com>
2024-09-13 17:29:12 -0700

    Added exclamation point to quail eggs

--- QuailEgg.gd
+++ QuailEgg.gd
@@ -6,17 +6,26 @@ extends Area2D
 @onready var sprite = $Sprite2D
 signal quail_hatched
 
+
+
 func _on_area_entered(area):
 	if area.is_in_group("player"):
+		$Exclaimation.show()
+		var tween = create_tween().set_loops()
+		tween.tween_property($Exclaimation, "position",Vector2(0,-15),0.2).from(Vector2(0,-9)).set_trans(Tween.TRANS_BOUNCE)
+		tween.tween_property($Exclaimation, "position",Vector2(0,-9),0.2).from(Vector2(0,-15)).set_trans(Tween.TRANS_BOUNCE)
 		timer.start()
 
 
 func _on_area_exited(area):
 	if area.is_in_group("player"):
+
+		$Exclaimation.hide()
 		timer.stop()
 
 
 func _on_timer_timeout():
+	$Exclaimation.hide()
 	sprite.visible = false
 	sound.play()
 	particles.emitting = true
